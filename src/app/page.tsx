244d9c08000b0b54d64d4caff1af3a47e2cdf487
Srnishappy <suranatsuwannarat@gmail.com>
2025-04-03 14:11:22 +0700

    db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,3 @@
-import { ModeToggle } from '@/components/ModeToggle';
-import { Button } from '@/components/ui/button';
-import {
-  SignedIn,
-  SignedOut,
-  SignInButton,
-  SignUpButton,
-  UserButton,
-} from '@clerk/nextjs';
-
 export default function Home() {
-  return (
-    <div className="m-4">
-      <SignedOut>
-        <SignInButton mode="modal" />
-        <Button> Sign in</Button>
-        <SignUpButton mode="modal" />
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-      <ModeToggle />
-    </div>
-  );
+  return <div></div>;
 }
