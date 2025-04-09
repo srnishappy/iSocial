e916ed50dcb736d15ce890ce731bee94a2fc4527
Srnishappy <suranatsuwannarat@gmail.com>
2025-04-09 12:25:54 +0700

    post like comment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,24 @@
+import { getPosts } from '@/actions/post.action';
 import CreatePost from '@/components/CreatePost';
+import PostCard from '@/components/PostCard';
 import WhoToFollow from '@/components/WhoToFollow';
 import { currentUser } from '@clerk/nextjs/server';
+import { getDbUserId } from '@/actions/user.action';
 
 export default async function Home() {
   const user = await currentUser();
+  const posts = await getPosts();
+  const dbUserId = await getDbUserId();
+
   return (
     <div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
       <div className="lg:col-span-6">
         {user ? <CreatePost /> : null}
 
         <div className="space-y-6">
-          {/* {posts.map((post) => (
-          <PostCard key={post.id} post={post} dbUserId={dbUserId} />
-        ))} */}
+          {posts.map((post) => (
+            <PostCard key={post.id} post={post} dbUserId={dbUserId} />
+          ))}
         </div>
       </div>
 
